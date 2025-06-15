e1b8c5f928c3bf1cc349267bf4d88f813c359948
prajwal <prajwalkreddy17@gmail.com>
2025-06-15 16:37:04 +0530

    added auth js & signIn

---
+++ apps/web/lib/auth.ts
@@ -0,0 +1,91 @@
+import NextAuth, { NextAuthConfig, NextAuthResult } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@repo/db/prisma"
+import bcrypt from "bcryptjs"
+
+export const config: NextAuthConfig = {
+  secret: process.env.AUTH_SECRET as string,
+  adapter: PrismaAdapter(prisma),
+  debug: process.env.NODE_ENV === "development",
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: "email",
+          label: "email",
+          placeholder: "Enter your email"
+        },
+        password: {
+          type: "password",
+          label: "Password",
+          placeholder: "*****",
+        },
+      },
+      authorize: async (credentials) => {
+        if (!credentials.email || !credentials.password) {
+          throw new Error("All fields are required")
+        };
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email
+          }
+        });
+
+        if (!user) {
+          throw new Error("User does not exist")
+        };
+
+        const isPassword = await bcrypt.compare(credentials.password as string, user.password)
+
+        if (!isPassword) {
+          throw new Error("Password is incorrect")
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    jwt: ({ token, user, account }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.access_token;
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+      }
+      return token
+    },
+    session: ({ token, session }) => {
+      if (token) {
+        session.user = {
+          ...session.user,
+          id: token.id as string,
+          name: token.name as string,
+          email: token.email as string
+        }
+        session.accessToken = token.accessToken as string
+        session.refreshToken = token.refreshToken as string
+      }
+      return session
+    }
+  }
+}
+
+export const authOptions = NextAuth(config)
+
+export const handlers: NextAuthResult["handlers"] = authOptions.handlers;
+export const auth: NextAuthResult["auth"] = authOptions.auth;
+export const signIn: NextAuthResult["signIn"] = authOptions.signIn;
+export const signOut: NextAuthResult["signOut"] = authOptions.signOut;
+
+// NextAuth export type fix - https://github.com/nextauthjs/next-auth/discussions/9950#discussioncomment-12937660
